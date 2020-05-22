a367c9c50618d9d67f0f802ee8d8b3504a882aa5
Onis brown <onis brown>
2020-05-22 01:32:14 +0100

    added human pose publishing and moved starting camera

--- collection/collection_pin.py
+++ collection/collection_pin.py
@@ -15,7 +15,12 @@ robot.translate(x=8.6, y=4, z=1)
 #robot.translate(x=1, y=7.62, z=0.0)
 robot.rotate(0, 0, 3.141592)
 human = Human()
+human.use_world_camera()
 human.translate(x=7, y=3, z=1)
+pose = Pose()
+human.append(pose)
+pose.add_stream('ros', method="morse.middleware.ros.pose.PoseStampedPublisher")
+
 
 # Battery discharging rate, in percent per seconds
 # The bateery state is published to /battery
